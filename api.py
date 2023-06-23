c88e8aa926ff944c7d4c6c66a2d33b046da22f9e
MansoorButtAnalog <mansoor.butt@analogmutations.com>
2023-06-23 19:23:22 +0500

    Update api.py

--- api.py
+++ api.py
@@ -7,6 +7,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+   return "Hello from Flask"
+
 @app.route("/csv", methods=["POST"])
 def getCsv():
   downloaded_file = "./file.csv"
